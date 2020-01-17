47b06804d17be570cb70362a738884809862b8b1
Giampaolo Eusebi <giampaolo.eusebi@gmail.com>
2020-01-17 04:39:38 +0100

    Add 'query' helper function

--- __init__.py
+++ __init__.py
@@ -5,4 +5,11 @@ from .vm import execute
 from .minidom_api import api as minidom_api
 
 __all__ = ["lexer", "parse", "compile", "execute",
-           "minidom_api", ]
+           "minidom_api", "query", ]
+
+
+def query(node, query):
+    tokens = lexer(query)
+    ast = parse(tokens)
+    code = compile(ast)
+    return execute(node, code, minidom_api)
