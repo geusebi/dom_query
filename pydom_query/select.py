97c5bbf66e6e8b010d40d32e31228081765e9dec
Giampaolo Eusebi <giampaolo.eusebi@gmail.com>
2020-01-24 11:56:02 +0100

    Add api to select-family function

--- pydom_query/select.py
+++ pydom_query/select.py
@@ -5,20 +5,20 @@ from .minidom_api import api as api
 __all__ = ["select_iter", "select", "select_all", ]
 
 
-def select_iter(node, query):
+def select_iter(node, query, api=api):
     """Generator which yield elements matched by `query`."""
     code = compile(query)
     return execute(node, code, api)
 
 
-def select(node, query):
+def select(node, query, api=api):
     """Return the first element matched by `query`."""
     try:
-        return next(select_iter(node, query))
+        return next(select_iter(node, query, api))
     except StopIteration:
         return None
 
 
-def select_all(node, query):
+def select_all(node, query, api=api):
     """Return every element matched by `query`."""
-    return tuple(select_iter(node, query))
+    return tuple(select_iter(node, query, api))
